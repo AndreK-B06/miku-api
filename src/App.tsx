ced4cc1b388fc9754a3c5bff236f4640603abbc1
André <137167063+AndreK-B06@users.noreply.github.com>
2024-06-13 11:18:08 +0200

    added section

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import FetchApi from "./Components/api/FetchApi";
 function App() {
   return (
     <>
-      <h1>Miku Fetch api</h1>
-      <h2>Fun facts about Miku</h2>
-      <FetchApi />
+      <section className="flex">
+        <h1>Miku Fetch api</h1>
+        <main className="flex">
+          <h2>Fun facts about Miku:</h2>
+          <FetchApi />
+        </main>
+      </section>
     </>
   );
 }
