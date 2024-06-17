59070e4ca3e7f153a41dec5ea982eb0d631783fe
André <137167063+AndreK-B06@users.noreply.github.com>
2024-06-17 14:05:38 +0200

    utilety and comet out for easy working

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import "./App.css";
 import FetchApi from "./Components/api/FetchApi";
-import FetchTimeLine from "./Components/api/FetchTimeLine";
+// import FetchTimeLine from "./Components/api/FetchTimeLine";
+import FetchVocaloids from "./Components/api/FetchVocaloids";
 
 function App() {
   return (
     <>
-      <section className="flex">
-        <main className="flex">
+      <section className="flex-column">
+        <main className="flex-column">
           <h1>Fun facts about Miku:</h1>
           <FetchApi />
         </main>
-        <section className="flex">
-          <FetchTimeLine />
+        <section className="flex-colum">
+          <FetchVocaloids />
         </section>
+        {/* Tamproary coment out code for esier work */}
+        {/* <section className="flex-column">
+          <FetchTimeLine />
+        </section> */}
       </section>
     </>
   );
