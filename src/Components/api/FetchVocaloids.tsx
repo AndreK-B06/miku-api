6d91a69e3a6ef06c84d70a0090f897599a1838c7
André <137167063+AndreK-B06@users.noreply.github.com>
2024-06-17 14:07:11 +0200

    maid FetchVocalooids

---
+++ src/Components/api/FetchVocaloids.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "../api Css/FetchVocaloids.css";
+
+export default function FetchVocaloids() {
+  const [vocaloids, setVocaloids] = useState<VocaloidsItem[]>([]);
+
+  interface VocaloidsItem {
+    name: string;
+    codename: string;
+    introduction: string;
+    Version: string;
+    image: string;
+  }
+
+  useEffect(() => {
+    const fetchVocaloids = async () => {
+      try {
+        const response = await fetch(
+          "https://leahjkh.github.io/MikuApiGithub/json/mikuVersions.json"
+        );
+        const vocaloidsData: VocaloidsItem[] = await response.json();
+        setVocaloids(vocaloidsData);
+        console.log(vocaloidsData);
+      } catch (error) {
+        console.error("Error fetching vocaloids data", error);
+      }
+    };
+
+    fetchVocaloids();
+  }, []);
+
+  return (
+    <div className="flex-row">
+      {vocaloids.map((item, index) => (
+        <div key={index} className="flex-column" id="vocaloids-cont">
+          <p>{item.name}</p>
+          <img src={item.image} alt={item.name} />
+          <p>{item.codename}</p>
+          <p>{item.introduction}</p>
+          <p>{item.Version}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
